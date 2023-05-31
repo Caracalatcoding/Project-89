2e681bf903079d339910efbd371f9e67200b9901
Caracalatcoding <113795210+Caracalatcoding@users.noreply.github.com>
2023-05-31 19:33:10 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,3 +1,10 @@
-// function AddUser(){}
-//Added after next Class.
-//By Aryaman Bhattcharyya 26.05.2023
+ function addUser()
+ {
+    player1 = document.getElementById("player1_name_input").value;
+    player2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("Player 1:", player1);
+    localStorage.setItem("Player 2:", player2);
+
+    window.location = "quiz_game_page.html";
+ }
